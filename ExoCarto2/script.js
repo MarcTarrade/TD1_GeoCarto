533b695ced9e12236f5dce8d7a2d641e5c30d37b
Marc Tarrade <marctarrade@gmail.com>
2021-01-06 12:16:19 +0100

    ex2

--- ExoCarto2/script.js
+++ ExoCarto2/script.js
@@ -12,22 +12,22 @@ function successWatch(position){
     console.log(position);
 }
 
-function successPos(position){
-    document.getElementById("latitude").innerHTML = position.coords.latitude;
-    document.getElementById("longitude").innerHTML = position.coords.longitude;
-    document.getElementById("precision").innerHTML = position.coords.accuracy;
-    if(position.coords.speed == null){
-        document.getElementById("vitesse").innerHTML = "Necessite de le faire sur téléphone";
-    }
-    else{
-        document.getElementById("vitesse").innerHTML = position.coords.speed;
-    }
-    console.log(position);
-}
+// function successPos(position){
+//     document.getElementById("latitude").innerHTML = position.coords.latitude;
+//     document.getElementById("longitude").innerHTML = position.coords.longitude;
+//     document.getElementById("precision").innerHTML = position.coords.accuracy;
+//     if(position.coords.speed == null){
+//         document.getElementById("vitesse").innerHTML = "Necessite de le faire sur téléphone";
+//     }
+//     else{
+//         document.getElementById("vitesse").innerHTML = position.coords.speed;
+//     }
+//     console.log(position);
+// }
 
 function erreur(err){
     console.warn(err);
 }
 
-navigator.geolocation.getCurrentPosition(successPos, erreur);
+//navigator.geolocation.getCurrentPosition(successPos, erreur);
 navigator.geolocation.watchPosition(successWatch, erreur);
