8593fe2de464d3777f61d5ab69efd165e99dbf0d
Marc Tarrade <marctarrade@gmail.com>
2021-01-06 12:18:00 +0100

    ex2

--- ExoCarto2/script.js
+++ ExoCarto2/script.js
@@ -1,33 +1,33 @@
 function successWatch(position){
-    document.getElementById("latitude").innerHTML = position.coords.latitude;
-    document.getElementById("longitude").innerHTML = position.coords.longitude;
-    document.getElementById("precision").innerHTML = position.coords.accuracy;
+    document.getElementById("latitude2").innerHTML = position.coords.latitude;
+    document.getElementById("longitude2").innerHTML = position.coords.longitude;
+    document.getElementById("precision2").innerHTML = position.coords.accuracy;
     if(position.coords.speed == null){
-        document.getElementById("vitesse").innerHTML = "Necessite de le faire sur téléphone";
+        document.getElementById("vitesse2").innerHTML = "Necessite de le faire sur téléphone";
     }
     else{
-        document.getElementById("vitesse").innerHTML = position.coords.speed;
+        document.getElementById("vitesse2").innerHTML = position.coords.speed;
     }
     document.getElementById("timeStamp").innerHTML = position.timestamp;
     console.log(position);
 }
 
-// function successPos(position){
-//     document.getElementById("latitude").innerHTML = position.coords.latitude;
-//     document.getElementById("longitude").innerHTML = position.coords.longitude;
-//     document.getElementById("precision").innerHTML = position.coords.accuracy;
-//     if(position.coords.speed == null){
-//         document.getElementById("vitesse").innerHTML = "Necessite de le faire sur téléphone";
-//     }
-//     else{
-//         document.getElementById("vitesse").innerHTML = position.coords.speed;
-//     }
-//     console.log(position);
-// }
+function successPos(position){
+    document.getElementById("latitude1").innerHTML = position.coords.latitude;
+    document.getElementById("longitude1").innerHTML = position.coords.longitude;
+    document.getElementById("precision1").innerHTML = position.coords.accuracy;
+    if(position.coords.speed == null){
+        document.getElementById("vitesse1").innerHTML = "Necessite de le faire sur téléphone";
+    }
+    else{
+        document.getElementById("vitesse1").innerHTML = position.coords.speed;
+    }
+    console.log(position);
+}
 
 function erreur(err){
     console.warn(err);
 }
 
-//navigator.geolocation.getCurrentPosition(successPos, erreur);
+navigator.geolocation.getCurrentPosition(successPos, erreur);
 navigator.geolocation.watchPosition(successWatch, erreur);
