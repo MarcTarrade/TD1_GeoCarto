6684be05b2223be83369e2937f4cbd7f8864981d
Marc Tarrade <marctarrade@gmail.com>
2021-01-06 10:59:54 +0100

    ex2

--- ExoCarto2/script.js
+++ ExoCarto2/script.js
@@ -2,7 +2,12 @@ function successPos(position){
     document.getElementById("latitude").innerHTML = position.coords.latitude;
     document.getElementById("longitude").innerHTML = position.coords.longitude;
     document.getElementById("precision").innerHTML = position.coords.accuracy;
-    document.getElementById("vitesse").innerHTML = position.coords.speed;
+    if(position.coords.speed == null){
+        document.getElementById("vitesse").innerHTML = "Necessite de le faire sur téléphone";
+    }
+    else{
+        document.getElementById("vitesse").innerHTML = position.coords.speed;
+    }
     document.getElementById("timeStamp").innerHTML = position.timestamp;
     console.log(position);
 }
