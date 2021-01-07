1ddd7e83958c55f574f3ba823bac2cb801982c2d
Marc Tarrade <marctarrade@gmail.com>
2021-01-07 08:38:32 +0100

    ex4

--- ExoCarto4/script.js
+++ ExoCarto4/script.js
@@ -1,6 +1,6 @@
 window.addEventListener("touchstart", success);
 
 function success(evt){
-    document.getElementById("touchPos").innerHTML = evt.pageX + " , " + evt.pageY;
-    document.getElementById("touchEvt").innerHTML = evt.type;
+    document.getElementById("touchPos").innerHTML = evt.touches.pageX + " , " + evt.pageY;
+    document.getElementById("touchEvt").innerHTML = evt.touches.type;
 }
