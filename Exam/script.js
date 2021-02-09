73aa364eb7a41cf630fab758c255d4a54203e539
Marc Tarrade <marctarrade@gmail.com>
2021-02-09 11:28:39 +0100

    update

--- Exam/script.js
+++ Exam/script.js
@@ -29,7 +29,7 @@ function drawCanvas(angle){
         fleche.onload = function(){
             dessin.drawImage(fleche, 0, 0);
             dessin.translate(100, 100);
-            dessin.rotate(angle * Math.PI / 180);
+            dessin.rotate(angle);
             dessin.translate(-100, -100);
             
         }
